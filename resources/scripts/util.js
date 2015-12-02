4d2d361ba63e74eac22fb9ecf6bc3820f1298ee8
Jesang Yoon <yoonjs2@kanizsalab.com>
2015-12-03 01:10:42 +0900

    remove jquery and add angular js

---
+++ resources/scripts/util.js
@@ -0,0 +1,17 @@
+/**
+ * Created by JesangYoon on 2015. 12. 3..
+ */
+
+function getURLParameter(name) {
+    var ret = null;
+    var nowAddress = unescape(window.location.href);
+    var parameters = (nowAddress.slice(nowAddress.indexOf("?") + 1, nowAddress.length)).split("&");
+    for (var i = 0; i < parameters.length; i++) {
+        var varName = parameters[i].split("=")[0];
+        if (varName.toUpperCase() === name.toUpperCase()) {
+            ret = parameters[i].split("=")[1];
+            break;
+        }
+    }
+    return ret;
+}
