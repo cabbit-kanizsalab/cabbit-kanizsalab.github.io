972dbe2eabad6307d0d6dc5830e2bf8aaeabde29
Jesang Yoon <yoonjs2@kanizsalab.com>
2016-01-08 00:52:37 +0900

    Reformat codes

--- app/support/scripts/util.js
+++ app/support/scripts/util.js
@@ -3,18 +3,18 @@
  */
 
 function getURLParameter(name) {
-    var ret = null;
-    var nowAddress = unescape(window.location.href);
-    var parameters = (nowAddress.slice(nowAddress.indexOf("?") + 1, nowAddress.length)).split("&");
-    for (var i = 0; i < parameters.length; i++) {
-        var varName = parameters[i].split("=")[0];
-        if (varName.toUpperCase() === name.toUpperCase()) {
-            ret = parameters[i].split("=")[1];
-            if (typeof(ret) === 'string') {
-                ret = ret.trim();
-            }
-            break;
-        }
+  var ret = null;
+  var nowAddress = unescape(window.location.href);
+  var parameters = (nowAddress.slice(nowAddress.indexOf("?") + 1, nowAddress.length)).split("&");
+  for (var i = 0; i < parameters.length; i++) {
+    var varName = parameters[i].split("=")[0];
+    if (varName.toUpperCase() === name.toUpperCase()) {
+      ret = parameters[i].split("=")[1];
+      if (typeof(ret) === 'string') {
+        ret = ret.trim();
+      }
+      break;
     }
-    return ret;
+  }
+  return ret;
 }
--- resources/scripts/util.js
+++ resources/scripts/util.js
@@ -1,20 +1,17 @@
-/**
- * Created by JesangYoon on 2015. 12. 3..
- */
 
 function getURLParameter(name) {
-    var ret = null;
-    var nowAddress = unescape(window.location.href);
-    var parameters = (nowAddress.slice(nowAddress.indexOf("?") + 1, nowAddress.length)).split("&");
-    for (var i = 0; i < parameters.length; i++) {
-        var varName = parameters[i].split("=")[0];
-        if (varName.toUpperCase() === name.toUpperCase()) {
-            ret = parameters[i].split("=")[1];
-            if (typeof(ret) === 'string') {
-                ret = ret.trim();
-            }
-            break;
-        }
+  var ret = null;
+  var nowAddress = unescape(window.location.href);
+  var parameters = (nowAddress.slice(nowAddress.indexOf("?") + 1, nowAddress.length)).split("&");
+  for (var i = 0; i < parameters.length; i++) {
+    var varName = parameters[i].split("=")[0];
+    if (varName.toUpperCase() === name.toUpperCase()) {
+      ret = parameters[i].split("=")[1];
+      if (typeof(ret) === 'string') {
+        ret = ret.trim();
+      }
+      break;
     }
-    return ret;
+  }
+  return ret;
 }
